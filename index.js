6118d0c6fdfa297355628249dad8416027d01d10
Hasibul0211 <mdhasibul0211@gmail.com>
2023-03-01 22:38:45 +0600

    commi

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ const cors = require('cors')
 const port = process.env.PORT || 5000
 
 app.use(cors())
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// })
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
     next();
-})
+});
 app.use(express.json())
 // database password = tq1PLsqIKMB2akVT
 
